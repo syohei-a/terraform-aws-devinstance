69290a482cb22e44386ee5797ab4f43a66a56b34
syohei-a <syohei_a@tokyo-gas.co.jp>
2024-05-17 06:44:25 +0000

    changed output

--- output.tf
+++ output.tf
@@ -1,3 +1,14 @@
-output "name" {
-  value = aws_eip.hashicat.public_ip
+output "eip" {
+  description = "AWSインスタンスのパブリックIPを表示"
+  value       = aws_eip.hashicat.public_ip
+}
+output "instance-info" {
+  description = "AWSインスタンスの情報を表示"
+  value = [
+    aws_instance.hashicat.arn,
+    aws_instance.hashicat.availability_zone,
+    aws_instance.hashicat.cpu_core_count,
+    aws_instance.hashicat.private_ip,
+    aws_instance.hashicat.instance_type
+  ]
 }
